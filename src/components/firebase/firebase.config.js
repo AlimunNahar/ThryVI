87ff9d20bbf7a3195998a72a0d36ff44d7228b9a
AlimunNahar <alimunnaharmitu@gmail.com>
2022-10-27 23:51:58 +0600

    final commit

--- src/components/firebase/firebase.config.js
+++ src/components/firebase/firebase.config.js
@@ -11,6 +11,12 @@ const firebaseConfig = {
   storageBucket: process.env.REACT_APP_storageBucket,
   messagingSenderId: process.env.REACT_APP_messagingSenderId,
   appId: process.env.REACT_APP_appId,
+  apiKey: "AIzaSyC5yYLODrd4BhcrwaP9oecegfdDd5riJYs",
+  authDomain: "assignment-10-e98fe.firebaseapp.com",
+  projectId: "assignment-10-e98fe",
+  storageBucket: "assignment-10-e98fe.appspot.com",
+  messagingSenderId: "307420214319",
+  appId: "1:307420214319:web:f6a37b016c3c5b963b82da",
 };
 
 // Initialize Firebase
